fc0e78f073af1c0e2ba4eac2f2fe7c1e41288e0a
sehoon123 <sehoon0519@naver.com>
2024-02-23 03:13:53 +0900

    게시물 목록 화면에서 사용자가 선택한 게시물 유형에 따라 게시물을 필터링하도록 수정함.

--- lib/screens/list_screen.dart
+++ lib/screens/list_screen.dart
@@ -13,45 +13,81 @@ class ListScreen extends StatefulWidget {
 
 class _ListScreenState extends State<ListScreen> {
   User? currentUser = FirebaseAuth.instance.currentUser;
+  bool isMyPostsSelected = true; // Default to show My Posts
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Posts'),
+        title: const Text('Posts'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('posts')
-            .where('userId', isEqualTo: currentUser!.uid)
-            .snapshots(), // Adjust the collection name if needed
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            List<Post> posts = snapshot.data!.docs
-                .map((doc) => Post.fromFirestore(doc))
-                .toList();
-            return ListView.builder(
-              itemCount: posts.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(posts[index].text),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PostDetailsScreen(
-                          post: posts[index], // Pass the whole Post object
-                        ),
-                      ),
-                    );
-                  },
-                );
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: ToggleButtons(
+              isSelected: [isMyPostsSelected, !isMyPostsSelected],
+              onPressed: (index) {
+                setState(() {
+                  isMyPostsSelected = index == 0;
+                });
               },
-            );
-          }
-        },
+              borderRadius: BorderRadius.circular(8.0),
+              selectedColor: Colors.white,
+              fillColor: Theme.of(context).primaryColor,
+              renderBorder: true,
+              constraints: const BoxConstraints(minHeight: 36, minWidth: 100),
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text('My Posts'),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text('Shared Posts'),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('posts')
+                  .where(
+                    isMyPostsSelected ? 'userId' : 'sharedUserId',
+                    isEqualTo: currentUser!.uid,
+                  )
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  List<Post> posts = snapshot.data!.docs
+                      .map((doc) => Post.fromFirestore(doc))
+                      .toList();
+                  return ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(posts[index].text),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PostDetailsScreen(
+                                post: posts[index],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
