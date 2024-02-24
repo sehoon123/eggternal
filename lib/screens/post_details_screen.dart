6ffb841b7c3ea4116a66324a539f8e15cf1db6b8
sehoon123 <sehoon0519@naver.com>
2024-02-24 18:55:51 +0900

    위치 초기화 및 이미지 뷰를 지도로 변경

--- lib/screens/post_details_screen.dart
+++ lib/screens/post_details_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:eggternal/models/post.dart';
 import 'package:eggternal/services/location_service.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +29,7 @@ class _PostDetailsScreenState extends State<PostDetailsScreen> {
     debugPrint('Initializing user location...');
 
     try {
-      debugPrint('location initialized: 0000');
+      debugPrint('location initialized:  0000');
       LatLng? location = await locationService.initializeMapCenter();
       debugPrint('location initialized: $location');
       if (location != null) {
@@ -46,7 +47,7 @@ class _PostDetailsScreenState extends State<PostDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     // Check if userLocation is null, show a loading indicator or handle accordingly
-    double distance = 100.0; // in meters
+    double distance =  100.0; // in meters
 
     if (isLoading) {
       return Scaffold(
@@ -70,7 +71,7 @@ class _PostDetailsScreenState extends State<PostDetailsScreen> {
     );
 
     // Set your threshold distance
-    double thresholdDistance = 30.0; // in meters
+    double thresholdDistance =  30.0; // in meters
 
     return Scaffold(
       appBar: AppBar(
@@ -82,7 +83,7 @@ class _PostDetailsScreenState extends State<PostDetailsScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(widget.post.text),
-            const SizedBox(height: 16.0),
+            const SizedBox(height:  16.0),
             // Display location information
             Text(
                 'Post Location: ${postLocation.latitude}, ${postLocation.longitude}'),
@@ -94,27 +95,29 @@ class _PostDetailsScreenState extends State<PostDetailsScreen> {
               Text(
                   'User Location: ${userLocation!.latitude}, ${userLocation!.longitude}'),
               // Display distance information
-              Text(
-                  'Distance from user: ${distance <= 1 ? '${(distance * 1000).toStringAsFixed(0)} meters' : '${distance.toStringAsFixed(2)} km'}'),
+              Text('Distance from user: ${distance <=   1 ? '${(distance *  1000).toStringAsFixed(0)} meters' : '${distance.toStringAsFixed(2)} km'}'),
               // Add additional information about the location as needed
-              const SizedBox(height: 16.0),
+              const SizedBox(height:  16.0),
+              // Replace the image view section with a map showing the post's location
               Expanded(
-                child: widget.post.imageUrls.isNotEmpty
-                    ? GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                        ),
-                        itemCount: widget.post.imageUrls.length,
-                        itemBuilder: (context, index) {
-                          return SizedBox(
-                            width: 200,
-                            height: 200,
-                            child: Image.network(widget.post.imageUrls[index]),
-                          );
-                        },
-                      )
-                    : const Center(child: Text('No Images')),
+                child: GoogleMap(
+                  myLocationButtonEnabled: true,
+                  myLocationEnabled: true,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(widget.post.location.latitude, widget.post.location.longitude),
+                    zoom:  14.0,
+                  ),
+                  markers: {
+                    Marker(
+                      markerId: MarkerId('postMarker'),
+                      position: LatLng(widget.post.location.latitude, widget.post.location.longitude),
+                      infoWindow: InfoWindow(
+                        title: 'Post Location',
+                        snippet: 'This is where the post is located.',
+                      ),
+                    ),
+                  },
+                ),
               ),
               // Check if the user is within the allowed distance
               if (distance <= thresholdDistance)
