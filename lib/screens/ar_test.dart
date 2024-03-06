7f589deed0ab5908ffe9245a1180d1704b1e2107
sehoon123 <sehoon0519@naver.com>
2024-03-07 05:11:00 +0900

    필요없는 주석 제거

--- lib/screens/ar_test.dart
+++ lib/screens/ar_test.dart
@@ -15,174 +15,159 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
 
 class ARViewPage extends StatefulWidget {
-  final Post post;
-  const ARViewPage({super.key, required this.post});
+ final Post post;
+ const ARViewPage({super.key, required this.post});
 
-  @override
-  _ARViewPageState createState() => _ARViewPageState();
+ @override
+ _ARViewPageState createState() => _ARViewPageState();
 }
 
 class _ARViewPageState extends State<ARViewPage> {
-  ARSessionManager? arSessionManager;
-  ARObjectManager? arObjectManager;
-  ARNode? localObjectNode;
-  String? messageToDisplay;
+ ARSessionManager? arSessionManager;
+ ARObjectManager? arObjectManager;
+ ARNode? localObjectNode;
+ String? messageToDisplay;
 
-  int count = 0;
+ int count = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    requestPermissions();
+ @override
+ void initState() {
+  super.initState();
+  requestPermissions();
+ }
+
+ Future<void> requestPermissions() async {
+  // Request camera permission for AR functionality
+  var status = await Permission.camera.request();
+  if (status.isGranted) {
+   debugPrint("Camera permission granted");
+   // Camera permission is granted, you can initialize AR functionalities here if needed
+  } else {
+   debugPrint("Camera permission denied");
+   // Handle the case where permission is not granted
   }
+ }
 
-  Future<void> requestPermissions() async {
-    // Request camera permission for AR functionality
-    var status = await Permission.camera.request();
-    if (status.isGranted) {
-      debugPrint("Camera permission granted");
-      // Camera permission is granted, you can initialize AR functionalities here if needed
-    } else {
-      debugPrint("Camera permission denied");
-      // Handle the case where permission is not granted
-    }
-  }
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+   appBar: AppBar(
+    title: const Text('AR Test'),
+   ),
+   body: Stack(
+    children: [
+     ARView(
+      onARViewCreated: onARViewCreated,
+     ),
+    ],
+   ),
+   floatingActionButton: FloatingActionButton(
+    onPressed: onLocalObjectAtOriginButtonPressed,
+    tooltip: 'Add Local Object',
+    child: const Icon(Icons.add),
+   ),
+  );
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AR Test'),
-      ),
-      body: Stack(
-        children: [
-          ARView(
-            onARViewCreated: onARViewCreated,
-          ),
-          // if (messageToDisplay != null)
-          //   Positioned(
-          //     top: 100,
-          //     left: 100,
-          //     child: Material(
-          //       color: Colors.transparent,
-          //       child: Text(
-          //         messageToDisplay!,
-          //         style: const TextStyle(
-          //           color: Colors.white,
-          //           fontSize: 24,
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: onLocalObjectAtOriginButtonPressed,
-        tooltip: 'Add Local Object',
-        child: const Icon(Icons.add),
-      ),
+ void onARViewCreated(
+   ARSessionManager arSessionManager,
+   ARObjectManager arObjectManager,
+   ARAnchorManager arAnchorManager,
+   ARLocationManager arLocationManager) {
+  this.arSessionManager = arSessionManager;
+  this.arObjectManager = arObjectManager;
+
+  this.arSessionManager!.onInitialize(
+     showFeaturePoints: false,
+     showPlanes: true,
+     customPlaneTexturePath: "triangle.png",
+     showWorldOrigin: false,
+     handleTaps: true,
     );
+  this.arObjectManager!.onInitialize();
+
+  this.arObjectManager!.onNodeTap = onARObjectTapped;
+ }
+
+ void onARObjectTapped(List<String> hits) {
+  debugPrint("AR object tapped $hits");
+
+  count += 1;
+
+  debugPrint(count.toString());
+
+  if (count >= 5) {
+   count = 0;
+   Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => DisplayPostScreen(post: widget.post)),
+   );
+  } else {
+   int remainingTaps = 5 - count;
+   Fluttertoast.showToast(
+    msg:
+      "Ouch, that hurts!, If you touch me $remainingTaps times, I'll get angry",
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.TOP,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.red,
+    textColor: Colors.white,
+    fontSize: 16.0,
+   );
+  }
+ }
+
+ void onLocalObjectAtOriginButtonPressed() async {
+  // Check if the ARObjectManager is initialized
+  if (arObjectManager == null) {
+   debugPrint("ARObjectManager is not initialized");
+   return;
   }
 
-  void onARViewCreated(
-      ARSessionManager arSessionManager,
-      ARObjectManager arObjectManager,
-      ARAnchorManager arAnchorManager,
-      ARLocationManager arLocationManager) {
-    this.arSessionManager = arSessionManager;
-    this.arObjectManager = arObjectManager;
+  // If the local object node already exists, remove it
+  if (localObjectNode != null) {
+   arObjectManager!.removeNode(localObjectNode!);
+   localObjectNode = null;
+   debugPrint("Local object removed");
+  } else {
+   // Assuming you have a method to get the current camera position and orientation
+   // This is a placeholder for such a method, which you might need to implement
+   // based on the AR framework you're using.
+   var cameraPosition = await getCurrentCameraPosition();
+   var cameraOrientation = await getCurrentCameraOrientation();
 
-    this.arSessionManager!.onInitialize(
-          showFeaturePoints: false,
-          showPlanes: true,
-          customPlaneTexturePath: "triangle.png",
-          showWorldOrigin: false,
-          handleTaps: true,
-        );
-    this.arObjectManager!.onInitialize();
+   var newPosition = cameraPosition + cameraOrientation * 1.0;
 
-    this.arObjectManager!.onNodeTap = onARObjectTapped;
+   // Create a new ARNode for the virtual object
+   var newNode = ARNode(
+    type: NodeType.localGLTF2,
+    uri: "assets/3D/Chicken_01/Chicken_01.gltf",
+    scale: vector.Vector3(0.2, 0.2, 0.2),
+    position: newPosition, // Position at the camera's current position
+    rotation: vector.Vector4(1.0, 0.0, 0.0, 0.0), // No rotation
+   );
+
+   // Add the new node to the ARObjectManager
+   bool? didAddLocalNode = await arObjectManager!.addNode(newNode);
+   if (didAddLocalNode == true) {
+    localObjectNode = newNode;
+    debugPrint("Local object added");
+   } else {
+    debugPrint("Failed to add local node");
+   }
   }
-
-  void onARObjectTapped(List<String> hits) {
-    debugPrint("AR object tapped $hits");
-
-    count += 1;
-
-    debugPrint(count.toString());
-
-    if (count >= 5) {
-      count = 0;
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => DisplayPostScreen(post: widget.post)),
-      );
-    } else {
-      int remainingTaps = 5 - count;
-      Fluttertoast.showToast(
-        msg:
-            "Ouch, that hurts!, If you touch me $remainingTaps times, I'll get angry",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.TOP,
-        timeInSecForIosWeb: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0,
-      );
-    }
-  }
-
-  void onLocalObjectAtOriginButtonPressed() async {
-    // Check if the ARObjectManager is initialized
-    if (arObjectManager == null) {
-      debugPrint("ARObjectManager is not initialized");
-      return;
-    }
-
-    // If the local object node already exists, remove it
-    if (localObjectNode != null) {
-      arObjectManager!.removeNode(localObjectNode!);
-      localObjectNode = null;
-      debugPrint("Local object removed");
-    } else {
-      // Assuming you have a method to get the current camera position and orientation
-      // This is a placeholder for such a method, which you might need to implement
-      // based on the AR framework you're using.
-      var cameraPosition = await getCurrentCameraPosition();
-      var cameraOrientation = await getCurrentCameraOrientation();
-
-      var newPosition = cameraPosition + cameraOrientation * 1.0;
-
-      // Create a new ARNode for the virtual object
-      var newNode = ARNode(
-        type: NodeType.localGLTF2,
-        uri: "assets/3D/Chicken_01/Chicken_01.gltf",
-        scale: vector.Vector3(0.2, 0.2, 0.2),
-        position: newPosition, // Position at the camera's current position
-        rotation: vector.Vector4(1.0, 0.0, 0.0, 0.0), // No rotation
-      );
-
-      // Add the new node to the ARObjectManager
-      bool? didAddLocalNode = await arObjectManager!.addNode(newNode);
-      if (didAddLocalNode == true) {
-        localObjectNode = newNode;
-        debugPrint("Local object added");
-      } else {
-        debugPrint("Failed to add local node");
-      }
-    }
-  }
+ }
 
 // Placeholder methods for getting the current camera position and orientation
-  Future<vector.Vector3> getCurrentCameraPosition() async {
-    // Implement this method based on your AR framework
-    // This should return the current position of the camera in the AR scene
-    return vector.Vector3(0.0, 0.0, 0.0); // Placeholder return value
-  }
+ Future<vector.Vector3> getCurrentCameraPosition() async {
+  // Implement this method based on your AR framework
+  // This should return the current position of the camera in the AR scene
+  return vector.Vector3(0.0, 0.0, 0.0); // Placeholder return value
+ }
 
-  Future<vector.Vector3> getCurrentCameraOrientation() async {
-    // Implement this method based on your AR framework
-    // This should return the current orientation of the camera in the AR scene
-    return vector.Vector3(0.0, 0.0, -1.0); // Placeholder return value
-  }
+ Future<vector.Vector3> getCurrentCameraOrientation() async {
+  // Implement this method based on your AR framework
+  // This should return the current orientation of the camera in the AR scene
+  return vector.Vector3(0.0, 0.0, -1.0); // Placeholder return value
+ }
 }
--- lib/screens/post_view.dart
+++ lib/screens/post_view.dart
@@ -5,129 +5,155 @@ import 'package:eggciting/models/post.dart';
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
 
 class DisplayPostScreen extends StatefulWidget {
-  final Post post;
+ final Post post;
 
-  const DisplayPostScreen({super.key, required this.post});
+ const DisplayPostScreen({super.key, required this.post});
 
-  @override
-  State<DisplayPostScreen> createState() => _DisplayPostScreenState();
+ @override
+ State<DisplayPostScreen> createState() => _DisplayPostScreenState();
 }
 
 class _DisplayPostScreenState extends State<DisplayPostScreen> {
-  fq.QuillController _controller = fq.QuillController.basic();
-  final ScrollController _scrollController = ScrollController();
-  double _imageCardHeight = 0;
-  double _imageOpacity = 1.0;
-  double _titleOpacity = 1.0; // For fading out title/location
-  final double _expandedHeight = 0.0;
+ fq.QuillController _controller = fq.QuillController.basic();
+ final ScrollController _scrollController = ScrollController();
+ double _imageCardHeight = 0;
+ double _imageOpacity = 1.0;
+ double _titleOpacity = 1.0; 
+ double _expandedHeight = 0.0; 
+ bool _imageLoaded = false;
 
-  @override
-  void initState() {
-    super.initState();
-    _loadContent();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      setState(() {
-        _imageCardHeight = MediaQuery.of(context).size.height * 0.8;
-      });
-    });
-    _scrollController.addListener(_onScroll);
-  }
+ @override
+ void initState() {
+  super.initState();
+  _loadContent();
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+   // Initial height calculation (might need further refinement)
+   setState(() { 
+    _expandedHeight = MediaQuery.of(context).size.height * 0.6; 
+   });
+  });
+  _scrollController.addListener(_onScroll);
+  _loadImage(); // Load the image
+ }
 
-  void _onScroll() {
-    setState(() {
-      double scrollOffset = _scrollController.offset;
+ void _onScroll() {
+  setState(() {
+   double scrollOffset = _scrollController.offset;
+   _imageOpacity = (1 - (scrollOffset / 300)).clamp(0.0, 1.0);
 
-      _imageOpacity = (1 - (scrollOffset / 300)).clamp(0.0, 1.0);
+   _titleOpacity = (1 - (scrollOffset / 100)).clamp(0.0, 1.0);
+  });
+ }
 
-      // Control the opacity of the title and location
-      _titleOpacity = (1 - (scrollOffset / 100)).clamp(0.0, 1.0);
-    });
-  }
+ Future<void> _loadContent() async {
+  final contentDelta = widget.post.contentDelta;
+  final doc = fq.Document.fromJson(jsonDecode(contentDelta));
+  debugPrint('Loaded content: $doc');
+  setState(() {
+   _controller = fq.QuillController(
+    document: doc,
+    selection: const TextSelection.collapsed(offset: 0),
+   );
+  });
+ }
 
-  Future<void> _loadContent() async {
-    final contentDelta = widget.post.contentDelta;
-    final doc = fq.Document.fromJson(jsonDecode(contentDelta));
-    debugPrint('Loaded content: $doc');
-    setState(() {
-      _controller = fq.QuillController(
-        document: doc,
-        selection: const TextSelection.collapsed(offset: 0),
-      );
-    });
-  }
+ void _loadImage() {
+  Image image = Image.network(widget.post.imageUrls.first); 
+  image.image.resolve(const ImageConfiguration()).addListener(
+   ImageStreamListener((info, _) {
+    if (!_imageLoaded) {
+     setState(() {
+      _expandedHeight = info.image.height.toDouble();
+      _imageLoaded = true;
+     });
+    }
+   }),
+  );
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onVerticalDragUpdate: (details) {
-          setState(() {
-            _imageCardHeight -= details.delta.dy;
-            _imageCardHeight = _imageCardHeight.clamp(
-                100.0, MediaQuery.of(context).size.height * 0.8);
-          });
-        },
-        child: CustomScrollView(
-          controller: _scrollController,
-          slivers: [
-            SliverAppBar(
-              expandedHeight: MediaQuery.of(context).size.height * 0.6,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Stack(
-                  children: [
-                    widget.post.imageUrls.isNotEmpty
-                        ? Image.network(
-                            widget.post.imageUrls.first,
-                            fit: BoxFit.cover,
-                          )
-                        : Container(),
-                    Positioned(
-                      bottom: 70.0,
-                      left: 20.0,
-                      child: Opacity(
-                        opacity: _titleOpacity,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              widget.post.title,
-                              style: const TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                            Text(
-                              widget.post.location.toString(),
-                              style: const TextStyle(
-                                  fontSize: 16, color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Container(
-                  color: Colors.white,
-                  child: fq.QuillEditor.basic(
-                    configurations: fq.QuillEditorConfigurations(
-                      controller: _controller,
-                      readOnly: true,
-                      showCursor: false,
-                      autoFocus: false,
-                      embedBuilders: FlutterQuillEmbeds.editorBuilders(),
-                    ),
-                  ),
-                ),
-              ]),
-            ),
-          ],
-        ),
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+   body: Stack( // Use a Stack for layering
+    children: [
+     Opacity(
+      opacity: _imageLoaded ? 0.0 : 0.5, 
+      child: Container(
+       color: Colors.black.withOpacity(0.5),
       ),
-    );
-  }
+     ),
+     const Center(
+      child: CircularProgressIndicator(),
+     ),
+     GestureDetector(
+      onVerticalDragUpdate: (details) { 
+       setState(() {
+        _imageCardHeight -= details.delta.dy;
+        _imageCardHeight = _imageCardHeight.clamp(
+         100.0, MediaQuery.of(context).size.height * 0.8);
+       });
+      },
+      child: CustomScrollView( 
+       controller: _scrollController,
+       slivers: [
+        SliverAppBar(
+         expandedHeight: _imageLoaded ? _expandedHeight : 200.0, 
+         flexibleSpace: FlexibleSpaceBar(
+          background: Stack(
+           children: [
+            widget.post.imageUrls.isNotEmpty 
+              ? _imageLoaded // Check the flag
+               ? Image.network( 
+                 widget.post.imageUrls.first,
+                 fit: BoxFit.cover,
+                )  
+               : Container() // No need for the circular indicator here
+              : Container(),
+            Positioned(
+             bottom: 70.0,
+             left: 20.0,
+             child: Opacity(
+              opacity: _titleOpacity,
+              child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                Text(
+                 widget.post.title,
+                 style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                Text(
+                 widget.post.location.toString(),
+                 style: const TextStyle(fontSize: 16, color: Colors.white),
+                ),
+               ],
+              ),
+             ),
+            ),
+           ],
+          ),
+         ),
+        ),
+        SliverList(
+         delegate: SliverChildListDelegate([
+          Container(
+           color: Colors.white,
+           child: fq.QuillEditor.basic(
+            configurations: fq.QuillEditorConfigurations(
+             controller: _controller,
+             readOnly: true,
+             showCursor: false,
+             autoFocus: false,
+             embedBuilders: FlutterQuillEmbeds.editorBuilders(),
+            ),
+           ),
+          ),
+         ]),
+        ),
+       ],
+      ),
+     ),
+    ],
+   ),
+  );
+ }
 }
