8b863d27b1211306ee6b9f7c698e887c4c8a8d58
sehoon123 <sehoon0519@naver.com>
2024-03-07 02:49:30 +0900

    DisplayPostScreen 코드 변경***
    ***DisplayPostScreen 코드 변경

--- lib/screens/post_view.dart
+++ lib/screens/post_view.dart
@@ -1,34 +1,39 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_quill/flutter_quill.dart' as fq;
 import 'package:eggciting/models/post.dart';
 import 'package:flutter_quill_extensions/flutter_quill_extensions.dart';
 
 class DisplayPostScreen extends StatefulWidget {
- final Post post;
+  final Post post;
 
- const DisplayPostScreen({Key? key, required this.post}) : super(key: key);
+  const DisplayPostScreen({super.key, required this.post});
 
- @override
- State<DisplayPostScreen> createState() => _DisplayPostScreenState();
+  @override
+  State<DisplayPostScreen> createState() => _DisplayPostScreenState();
 }
 
 class _DisplayPostScreenState extends State<DisplayPostScreen> {
- fq.QuillController _controller = fq.QuillController.basic();
- double _imageCardHeight = 0; // Initially set to 0 to avoid rendering issues
+  fq.QuillController _controller = fq.QuillController.basic();
 
- @override
- void initState() {
+  ScrollController _scrollController = ScrollController();
+  double _imageCardHeight = 0; 
+
+
+  @override
+  void initState() {
     super.initState();
     _loadContent();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
-        _imageCardHeight = MediaQuery.of(context).size.height * 0.8; // Set initial height after build
+        _imageCardHeight = MediaQuery.of(context).size.height *
+            0.8; // Set initial height after build
       });
     });
- }
+  }
 
- Future<void> _loadContent() async {
+  Future<void> _loadContent() async {
     final contentDelta = widget.post.contentDelta;
     final doc = fq.Document.fromJson(jsonDecode(contentDelta));
     debugPrint('Loaded content: $doc');
@@ -38,10 +43,10 @@ class _DisplayPostScreenState extends State<DisplayPostScreen> {
         selection: const TextSelection.collapsed(offset: 0),
       );
     });
- }
+  }
 
- @override
- Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.post.title, style: const TextStyle(fontSize: 30)),
@@ -49,8 +54,12 @@ class _DisplayPostScreenState extends State<DisplayPostScreen> {
       body: GestureDetector(
         onVerticalDragUpdate: (details) {
           setState(() {
-            _imageCardHeight -= details.delta.dy; // Adjust the height based on the swipe
-            _imageCardHeight = _imageCardHeight.clamp(100.0, MediaQuery.of(context).size.height * 0.8); // Ensure the height stays within bounds
+            _imageCardHeight -=
+                details.delta.dy; // Adjust the height based on the swipe
+            _imageCardHeight = _imageCardHeight.clamp(
+                100.0,
+                MediaQuery.of(context).size.height *
+                    0.8); // Ensure the height stays within bounds
           });
         },
         child: Stack(
@@ -59,25 +68,30 @@ class _DisplayPostScreenState extends State<DisplayPostScreen> {
               height: _imageCardHeight,
               duration: const Duration(milliseconds: 300),
               child: widget.post.imageUrls.isNotEmpty
-                 ? Image.network(
-                      widget.post.imageUrls.first, // Assuming the first image is the main one
+                  ? Image.network(
+                      widget.post.imageUrls
+                          .first, // Assuming the first image is the main one
                       fit: BoxFit.cover,
                     )
-                 : Container(), // Empty container if no images
+                  : Container(), // Empty container if no images
             ),
             SingleChildScrollView(
               child: Column(
                 children: [
-                 SizedBox(height: _imageCardHeight), // Space for the image card
-                 fq.QuillEditor.basic(
-                    configurations: fq.QuillEditorConfigurations(
-                      controller: _controller,
-                      readOnly: true,
-                      showCursor: false,
-                      autoFocus: false,
-                      embedBuilders: FlutterQuillEmbeds.editorBuilders(),
+                  SizedBox(
+                      height: _imageCardHeight), // Space for the image card
+                  Container(
+                    color: Colors.white,
+                    child: fq.QuillEditor.basic(
+                      configurations: fq.QuillEditorConfigurations(
+                        controller: _controller,
+                        readOnly: true,
+                        showCursor: false,
+                        autoFocus: false,
+                        embedBuilders: FlutterQuillEmbeds.editorBuilders(),
+                      ),
                     ),
-                 ),
+                  ),
                 ],
               ),
             ),
@@ -85,5 +99,5 @@ class _DisplayPostScreenState extends State<DisplayPostScreen> {
         ),
       ),
     );
- }
+  }
 }
